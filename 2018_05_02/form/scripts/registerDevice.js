4a58c9722ae680ad95e8c1f16b96b5ac3bb395f1
swordfishwjy <swordfishwjy@gmail.com>
2018-05-03 15:07:23 -0500

    udpate

--- 2018_05_02/form/scripts/registerDevice.js
+++ 2018_05_02/form/scripts/registerDevice.js
@@ -1,31 +1,27 @@
 var DeviceReg = artifacts.require("DeviceReg");
 
-module.exports = function(callback){
-	var instance;
-    var t = [1];
-    var addrHead = "0x";
-    var addrTail = 0;
-    var totalNum = 4;
-    // var instance;
-
-    var i;
-    for(i=0; i < totalNum; i++){
-    	addrTail = addrTail + 1;
-    	DeviceReg.deployed().then(function(rinst){
-	        instance = rinst;
-	        console.log(addrTail);
-	     //    return instance.registerDevice(
-		    //     "0x" + addrTail.toString(),
-		    //     2,
-		    //     101,
-		    //     t,
-		    //     t,
-		    //     [],
-		    //     "MA:CA:DD:RE:SS",
-		    //     {from: "0xd15837424997020a0870ef4c43efa2714e184bbf"}
-		    // ); 
+function myfunction(a){
+	DeviceReg.deployed().then(function(rinst){
+	        return rinst.registerDevice(
+		        "0x"+a.toString(),
+		        2,
+		        101,
+		        [1],
+		        [1],
+		        [],
+		        "MA:CA:DD:RE:SS",
+		        {from: "0xc968efa8019d670db8a6a3da22f30613db6a8447"}
+		    ); 
 		});
-		console.log(addrTail);
+}
+
+module.exports = function(callback){
+    var totalNum = 1000;
+    // myfunction();
+    var i;
+    for(i=1; i <= totalNum; i++){
+		// console.log(i);
+		myfunction(i);
     }
 
     
